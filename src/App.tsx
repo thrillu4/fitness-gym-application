92198bd4d8b6ef33381f38e8225b6cb30e1a0632
Denis <tonopik@gmail.com>
2023-03-24 22:33:21 +0200

    create a navbar with a links + mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import Navbar from "@/components/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
 function App() {
-  return <div className="App">Hi</div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div>
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
 }
 
 export default App;
